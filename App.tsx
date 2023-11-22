4a2cdfd496a77c62e52584c5004ce5acb7159824
Lucas-Masaba <masabaluke@gmail.com>
2023-11-22 20:48:36 +0300

    Initialize rate repository app

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello There. Looking forward to learning mobile app development</Text>
       <StatusBar style="auto" />
     </View>
   );
