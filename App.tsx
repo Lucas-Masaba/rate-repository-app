d3b9e1a115fa3335a1e4dbe623200a6d59256b38
Lucas-Masaba <masabaluke@gmail.com>
2023-11-24 01:14:09 +0300

    scre expo

--- App.tsx
+++ App.tsx
@@ -1,21 +1,9 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import Main from './src/components/Main';
 
-export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text>Hello There. Looking forward to learning mobile app development</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
-}
+const App = () => {
+  return <Main />;
+  
+};
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
+export default App;
